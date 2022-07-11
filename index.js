4636107f9ebf290a403fd71cf0318db63bb21973
Antonio Isaac Ramirez Monsalve <metal_invaders_21@hotmail.com>
2022-07-10 23:29:08 -0400

    commitDeploy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path') 
 const { dbConnection } = require('./DB/config');
 require('dotenv').config();//esto sirve para cargar las variables de entorno del archivo .env ubicado raiz del proyecto
 
@@ -28,6 +29,13 @@ app.use(cors());
 
 app.use('/api/auth', require('./routes/auth.routes'))
 
+//manejar demas rutas
+//este codigo fue insertado para poder manejar las rutas una vez que al applicacion de angular ya fue copiada en la carpeta public 
+app.get('*',(req,res) =>{
+res.sendFile(path.resolve(__dirname,'public/index.html'))
+})
+
+//configuracion del puerto en el que trabajara el servidor
 app.listen(process.env.PORT, () =>{
     console.log(`el servidor corre en el puerto ${process.env.PORT}`)
 })
